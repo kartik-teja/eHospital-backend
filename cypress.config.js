0c00d5ccf6e743ccaf3ad3716cb209595d912c01
kartik-teja <kartikteja343@gmail.com>
2024-07-24 21:28:45 +0530

    update

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+    e2e: {
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
+        baseUrl: 'http://localhost:4000', // base URL for your application
+        specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', // pattern to match test files
+    },
+})
